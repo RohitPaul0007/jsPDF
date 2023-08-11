17b2aaa870074c3f06f283abb0a7d2491b5dd9ee
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-11 20:50:24 +0530

    Update outline.spec.js

--- test/specs/outline.spec.js
+++ test/specs/outline.spec.js
@@ -9,7 +9,7 @@
 describe("Module: Outline", () => {
   beforeAll(loadGlobals);
   it("should create a bookmark in a pdf generated with units in points", () => {
-    var doc = new jsPDF({ unit: "pt", floatPrecision: 2 });
+    let doc = new jsPDF({ unit: "pt", floatPrecision: 2 });
     doc.outline.add(null, "Page 1", { pageNumber: 1 });
     doc.addPage();
 
@@ -18,7 +18,7 @@ describe("Module: Outline", () => {
 
   // @TODO: Document
   it("should create a bookmark in a pdf generated with units in inches", () => {
-    var doc = new jsPDF({ unit: "in", floatPrecision: 2 });
+    let doc = new jsPDF({ unit: "in", floatPrecision: 2 });
     doc.outline.add(null, "Page 1", { pageNumber: 1 });
     doc.addPage();
 
@@ -27,7 +27,7 @@ describe("Module: Outline", () => {
 
   // @TODO: Document
   it("should create a bookmark in a pdf generated with units in mm", () => {
-    var doc = new jsPDF({ unit: "mm", floatPrecision: 2 });
+    let doc = new jsPDF({ unit: "mm", floatPrecision: 2 });
     doc.outline.add(null, "Page 1", { pageNumber: 1 });
     doc.addPage();
 
