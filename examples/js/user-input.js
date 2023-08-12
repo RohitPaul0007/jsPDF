1b09ee79662e582671192de93ebd0befe1f222c0
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-12 18:08:15 +0530

    Update user-input.js

--- examples/js/user-input.js
+++ examples/js/user-input.js
@@ -1,13 +1,13 @@
-var name = prompt("What is your name?");
-var multiplier = parseInt(prompt("Enter a number:"), 10);
+let name = prompt("What is your name?");
+let multiplier = parseInt(prompt("Enter a number:"), 10);
 
-var doc = new jsPDF();
+let doc = new jsPDF();
 doc.setFontSize(22);
 doc.text(20, 20, "Questions");
 doc.setFontSize(16);
 doc.text(20, 30, "This belongs to: " + name);
 
-for (var i = 1; i <= 12; i++) {
+for (let i = 1; i <= 12; i++) {
   doc.text(20, 30 + i * 10, i + " x " + multiplier + " = ___");
 }
 
@@ -16,7 +16,7 @@ doc.setFontSize(22);
 doc.text(20, 20, "Answers");
 doc.setFontSize(16);
 
-for (var i = 1; i <= 12; i++) {
+for (let i = 1; i <= 12; i++) {
   doc.text(20, 30 + i * 10, i + " x " + multiplier + " = " + i * multiplier);
 }
 
