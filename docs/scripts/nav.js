a43153c586b8543632d7bd24e88daf403a4dc2e6
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-14 12:59:12 +0530

    Update nav.js

--- docs/scripts/nav.js
+++ docs/scripts/nav.js
@@ -1,7 +1,7 @@
 function scrollToNavItem() {
-    var path = window.location.href.split('/').pop().replace(/\.html/, '');
+    let path = window.location.href.split('/').pop().replace(/\.html/, '');
     document.querySelectorAll('nav a').forEach(function(link) {
-      var href = link.attributes.href.value.replace(/\.html/, '');
+      let href = link.attributes.href.value.replace(/\.html/, '');
       if (path === href) {
         link.scrollIntoView({block: 'center'});
         return;
